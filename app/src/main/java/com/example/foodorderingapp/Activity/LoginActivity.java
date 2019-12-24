f451dfadf0895bf33d2e1661f60316e8db9da825
ashishpokhrel123 <aashishpokhrel146@gmail.com>
2019-12-24 21:56:54 +0545

    Login and registration form design compelete

--- app/src/main/java/com/example/foodorderingapp/Activity/LoginActivity.java
+++ app/src/main/java/com/example/foodorderingapp/Activity/LoginActivity.java
@@ -2,11 +2,16 @@ package com.example.foodorderingapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.foodorderingapp.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView txtnext;
 
 
 
@@ -16,6 +21,17 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().hide();
+
+        txtnext = findViewById(R.id.txtnext);
+
+        txtnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(i);
+            }
+        });
 
 
 
